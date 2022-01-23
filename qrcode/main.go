34e656b515a9a5e863b5c7505b90f19c8c6d42e9
Robert Peters <peters.robert.j@gmail.com>
2022-01-23 12:23:13 -0600

    Package stuff temporarily

--- qrcode/main.go
+++ qrcode/main.go
@@ -3,97 +3,97 @@
 
 package main
 
-import (
-	"flag"
-	"fmt"
-	"log"
-	"os"
-	"strings"
-
-	qrcode "github.com/namsor/go-qrcode"
-)
-
-func main() {
-	outFile := flag.String("o", "", "out PNG file prefix, empty for stdout")
-	outFormat := flag.String("f", "png", "output format PNG(default) or SVG")
-	size := flag.Int("s", 256, "image size (pixel)")
-	textArt := flag.Bool("t", false, "print as text-art on stdout")
-	negative := flag.Bool("i", false, "invert black and white")
-	disableBorder := flag.Bool("d", false, "disable QR Code border")
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, `qrcode -- QR Code encoder in Go
-https://github.com/skip2/go-qrcode
-
-Flags:
-`)
-		flag.PrintDefaults()
-		fmt.Fprintf(os.Stderr, `
-Usage:
-  1. Arguments except for flags are joined by " " and used to generate QR code.
-     Default output is STDOUT, pipe to imagemagick command "display" to display
-     on any X server.
-
-       qrcode hello word | display
-
-  2. Save to file if "display" not available:
-
-       qrcode "homepage: https://github.com/skip2/go-qrcode" > out.png
-
-`)
-	}
-	flag.Parse()
-
-	if len(flag.Args()) == 0 {
-		flag.Usage()
-		checkError(fmt.Errorf("Error: no content given"))
-	}
-
-	content := strings.Join(flag.Args(), " ")
-
-	var err error
-	var q *qrcode.QRCode
-	q, err = qrcode.New(content, qrcode.Highest)
-	checkError(err)
-
-	if *disableBorder {
-		q.DisableBorder = true
-	}
-
-	if *textArt {
-		art := q.ToString(*negative)
-		fmt.Println(art)
-		return
-	}
-
-	if *negative {
-		q.ForegroundColor, q.BackgroundColor = q.BackgroundColor, q.ForegroundColor
-	}
-
-
-	var png []byte
-	if *outFormat=="png" {
-		png, err = q.PNG(*size)
-		checkError(err)
-	} else if *outFormat=="svg" {
-		log.Print("Creating a SVG")
-		qrSVG,_ := q.SVG()
-		fmt.Printf("SVG= %s \n", qrSVG)
-	}
-
-	if *outFile == "" {
-		os.Stdout.Write(png)
-	} else {
-		var fh *os.File
-		fh, err = os.Create(*outFile + "."+ *outFormat)
-		checkError(err)
-		defer fh.Close()
-		fh.Write(png)
-	}
-}
-
-func checkError(err error) {
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-		os.Exit(1)
-	}
-}
+//import (
+//	"flag"
+//	"fmt"
+//	"log"
+//	"os"
+//	"strings"
+//
+//	qrcode "github.com/namsor/go-qrcode"
+//)
+//
+//func main() {
+//	outFile := flag.String("o", "", "out PNG file prefix, empty for stdout")
+//	outFormat := flag.String("f", "png", "output format PNG(default) or SVG")
+//	size := flag.Int("s", 256, "image size (pixel)")
+//	textArt := flag.Bool("t", false, "print as text-art on stdout")
+//	negative := flag.Bool("i", false, "invert black and white")
+//	disableBorder := flag.Bool("d", false, "disable QR Code border")
+//	flag.Usage = func() {
+//		fmt.Fprintf(os.Stderr, `qrcode -- QR Code encoder in Go
+//https://github.com/skip2/go-qrcode
+//
+//Flags:
+//`)
+//		flag.PrintDefaults()
+//		fmt.Fprintf(os.Stderr, `
+//Usage:
+//  1. Arguments except for flags are joined by " " and used to generate QR code.
+//     Default output is STDOUT, pipe to imagemagick command "display" to display
+//     on any X server.
+//
+//       qrcode hello word | display
+//
+//  2. Save to file if "display" not available:
+//
+//       qrcode "homepage: https://github.com/skip2/go-qrcode" > out.png
+//
+//`)
+//	}
+//	flag.Parse()
+//
+//	if len(flag.Args()) == 0 {
+//		flag.Usage()
+//		checkError(fmt.Errorf("Error: no content given"))
+//	}
+//
+//	content := strings.Join(flag.Args(), " ")
+//
+//	var err error
+//	var q *qrcode.QRCode
+//	q, err = qrcode.New(content, qrcode.Highest)
+//	checkError(err)
+//
+//	if *disableBorder {
+//		q.DisableBorder = true
+//	}
+//
+//	if *textArt {
+//		art := q.ToString(*negative)
+//		fmt.Println(art)
+//		return
+//	}
+//
+//	if *negative {
+//		q.ForegroundColor, q.BackgroundColor = q.BackgroundColor, q.ForegroundColor
+//	}
+//
+//
+//	var png []byte
+//	if *outFormat=="png" {
+//		png, err = q.PNG(*size)
+//		checkError(err)
+//	} else if *outFormat=="svg" {
+//		log.Print("Creating a SVG")
+//		qrSVG,_ := q.SVG()
+//		fmt.Printf("SVG= %s \n", qrSVG)
+//	}
+//
+//	if *outFile == "" {
+//		os.Stdout.Write(png)
+//	} else {
+//		var fh *os.File
+//		fh, err = os.Create(*outFile + "."+ *outFormat)
+//		checkError(err)
+//		defer fh.Close()
+//		fh.Write(png)
+//	}
+//}
+//
+//func checkError(err error) {
+//	if err != nil {
+//		fmt.Fprintf(os.Stderr, "%s\n", err)
+//		os.Exit(1)
+//	}
+//}
